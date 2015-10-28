68494747e204ef1e47eb2edcbc022b79b849779a
Hemal Harindaka Athulathmudali <harindaka@gmail.com>
2015-10-28 21:05:17 +0530

    added test routing logic to controller factory hook

--- api/hooks/controller-factory-hook/index.js
+++ api/hooks/controller-factory-hook/index.js
@@ -3,12 +3,18 @@
  */
 
 module.exports = function (sails) {
-  return {
+  var hook = {};
+
+  hook.routes = { before: {} };
+  var controllers = hook.routes.before;
+  controllers["GET /api/Users"] = function(req, res, next) {
+    res.json({Username: 'John', FirstName: 'John', LastName: 'Doe'});
+  };
 
     // Run when sails loads-- be sure and call `next()`.
-    initialize: function (next) {
-      return next();
-    }
+    //initialize: function (next) {
+      //return next();
+    //}
 
-  };
+  return hook;
 };
